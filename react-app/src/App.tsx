57cf317c74d004cd4ca71df2507c2a97143c7704
Kyle Marek <kmarek1530@gmail.com>
2020-11-25 23:29:56 -0600

    Finished inputs. Fixed icons.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -13,22 +13,24 @@ import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
+import logo from './images/LogoPng.png';
+
 const useStyles = makeStyles((theme: Theme) => ({
   mainContent: {
-    paddingTop: theme.spacing(6),
-    paddingBottom: theme.spacing(6)
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
   },
   heroContent: {
-    padding: theme.spacing(6, 0, 4),
+    padding: theme.spacing(4, 0, 4),
   },
   body: {
     backgroundColor: theme.palette.background.paper,
   },
   paper: {
     padding: theme.spacing(2),
-/*     display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column', */
+    /*     display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column', */
   },
   button: {
     marginTop: theme.spacing(2),
@@ -43,7 +45,11 @@ function App() {
   const classes = useStyles();
 
   const [yearsInv, setYearsInv] = useState<String>('');
+  const [yearsRet, setYearsRet] = useState<String>('');
   const [inv, setInv] = useState<String>('');
+  const [retPct, setRetPct] = useState<String>('');
+  const [divPct, setDivPct] = useState<String>('');
+  const [divGrowth, setDivGrowth] = useState<String>('');
 
   const handleYearsInv = (event: any) => {
     const re = /^[1-9]{1}$|^[1-9]{1}[0-9]{1}$/;
@@ -52,35 +58,75 @@ function App() {
     }
   };
 
+  const handleYearsRet = (event: any) => {
+    const re = /^[0-9]{1}$|^[1-9]{1}[0-9]{1}$/;
+    if (event.target.value === '' || re.test(event.target.value)) {
+      setYearsRet(event.target.value);
+    }
+  };
+
   const handleInv = (event: any) => {
-    const re = /^\d{0,2}(\.\d{1,2})?$/;
+    const re = /^[1-9]{1}$|^[1-9]{1}[0-9]{1,5}$/;
     if (event.target.value === '' || re.test(event.target.value)) {
       setInv(event.target.value);
     }
   };
 
+  const handleRetPct = (event: any) => {
+    const re = /^[0-9]{1}$|^[0-9]{1}\.{1}$|^[0-9]{1}\.{1}[0-9]{1,2}$|^[1-9]{1}[0-9]{1}$|^[1-9]{1}[0-9]{1}\.{1}$|^[1-9]{1}[0-9]{1}\.{1}[0-9]{1,2}$/;
+    if (event.target.value === '' || re.test(event.target.value)) {
+      setRetPct(event.target.value);
+    }
+  };
+
+  const handleDivPct = (event: any) => {
+    const re = /^[0-9]{1}$|^[0-9]{1}\.{1}$|^[0-9]{1}\.{1}[0-9]{1,2}$|^[1-9]{1}[0-9]{1}$|^[1-9]{1}[0-9]{1}\.{1}$|^[1-9]{1}[0-9]{1}\.{1}[0-9]{1,2}$/;
+    if (event.target.value === '' || re.test(event.target.value)) {
+      setDivPct(event.target.value);
+    }
+  };
+
+  const handleDivGrowth = (event: any) => {
+    const re = /^[0-9]{1}$|^[0-9]{1}\.{1}$|^[0-9]{1}\.{1}[0-9]{1,2}$|^[1-9]{1}[0-9]{1}$|^[1-9]{1}[0-9]{1}\.{1}$|^[1-9]{1}[0-9]{1}\.{1}[0-9]{1,2}$/;
+    if (event.target.value === '' || re.test(event.target.value)) {
+      setDivGrowth(event.target.value);
+    }
+  };
 
   const submitForm = (event: any) => {
     event.preventDefault();
+
+    calculate(Number(yearsInv), Number(yearsRet), Number(inv), Number(retPct), Number(divPct), Number(divGrowth));
   }
   /*
   Improvements
-    * put separate card for inputs with fields and calculate button
-    * put separate card for graph
     * put separate card for data grid with the data from each year
+    * add graph
+    * add total number
+    * allow for any variation of investing/retired with inputs
   */
-  
+
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
+      <CssBaseline />
       <div className={classes.heroContent}>
         <Container maxWidth="sm">
-          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Investment Return Calculator
-          </Typography>
-          <Typography variant="h5" align="center" color="textSecondary" paragraph>
-            Calculate the yearly return of your investments. Made by Kyle Marek.
-          </Typography>
+          <Grid
+            container
+            justify="center"
+          >
+            <img
+              src={logo}
+              width="100px"
+              alt="logo"
+            />
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Investment Return Calculator
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Calculate the year by year return of your investments. Made by Kyle Marek.
+            </Typography>
+          </Grid>
         </Container>
       </div>
       <Container maxWidth="md" className={classes.mainContent}>
@@ -91,16 +137,15 @@ function App() {
         >
           <Grid item xs={12}>
             <Paper className={classes.paper}>
-              <Grid
-                container
-                justify="center"
-                spacing={3}
-              >
-                <Grid item xs={6}>
-                  <form className={classes.formMargin} onSubmit={submitForm} autoComplete="off">
+              <form onSubmit={submitForm} autoComplete="off">
+                <Grid
+                  container
+                  justify="center"
+                  spacing={3}
+                >
+                  <Grid item xs={6}>
                     <TextField
                       color={"secondary"}
-                      margin="normal"
                       required
                       fullWidth
                       id="yearsInv"
@@ -112,14 +157,28 @@ function App() {
                         startAdornment: <InputAdornment position="start"> </InputAdornment>,
                       }}
                       autoFocus
+                      helperText="Years adding investments (1 - 99)"
                     />
-                  </form>
-                </Grid>
-                <Grid item xs={6}>
-                  <form className={classes.formMargin} onSubmit={submitForm} autoComplete="off">
+                  </Grid>
+                  <Grid item xs={6}>
                     <TextField
                       color={"secondary"}
-                      margin="normal"
+                      required
+                      fullWidth
+                      id="yearsRet"
+                      label="Years Retired"
+                      name="yearsRet"
+                      value={yearsRet}
+                      onChange={handleYearsRet}
+                      InputProps={{
+                        startAdornment: <InputAdornment position="start"> </InputAdornment>,
+                      }}
+                      helperText="Years without adding investments (0 - 99)"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      color={"secondary"}
                       required
                       fullWidth
                       id="inv"
@@ -130,17 +189,67 @@ function App() {
                       InputProps={{
                         startAdornment: <InputAdornment position="start">$</InputAdornment>,
                       }}
+                      helperText="Investment added per year ($1 - 999,999)"
                     />
-                  </form>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      color={"secondary"}
+                      required
+                      fullWidth
+                      id="retPct"
+                      label="Average Yearly Return"
+                      name="retPct"
+                      value={retPct}
+                      onChange={handleRetPct}
+                      InputProps={{
+                        startAdornment: <InputAdornment position="start">%</InputAdornment>,
+                      }}
+                      helperText="Percent return expected yearly (.01 - 99.99%)"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      color={"secondary"}
+                      required
+                      fullWidth
+                      id="divPct"
+                      label="Average Dividend Percentage"
+                      name="divPct"
+                      value={divPct}
+                      onChange={handleDivPct}
+                      InputProps={{
+                        startAdornment: <InputAdornment position="start">%</InputAdornment>,
+                      }}
+                      helperText="Percent dividend return expected yearly (.01 - 99.99%)"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      color={"secondary"}
+                      required
+                      fullWidth
+                      id="divGrowth"
+                      label="Average Dividend Growth Percentage"
+                      name="divGrowth"
+                      value={divGrowth}
+                      onChange={handleDivGrowth}
+                      InputProps={{
+                        startAdornment: <InputAdornment position="start">%</InputAdornment>,
+                      }}
+                      helperText="Percent dividend growth expected yearly (.01 - 99.99%)"
+                    />
+                  </Grid>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    className={classes.button}
+                  /* onClick={() => calculate(20, 40000, 20, 10, 3, 7)} */
+                  >
+                    Calculate
+                  </Button>
                 </Grid>
-                <Button 
-                  variant="contained"
-                  className={classes.button}
-                  onClick={() => calculate(20, 40000, 20, 10, 3, 7)}
-                >
-                  Calculate
-                </Button>
-              </Grid>
+              </form>
             </Paper>
           </Grid>
           <Grid item xs={12} md={12} lg={2}>
--- react-app/src/functions/functions.ts
+++ react-app/src/functions/functions.ts
@@ -2,8 +2,8 @@
 
 /*
 yearsInv = years investing (adding money)
-inv = investment per year
 yearsRet = years not investing (not adding money)
+inv = investment per year
 retPct = average return percentage yearly
 divPct = average dividend percentage yearly
 divGrowth = average dividend growth percentage yearly
@@ -12,21 +12,26 @@ yearlyTotals = the total at the end of each year
 yearlyInvestments = the amount your money from each year is worth
 */
 
-export default function calculate(yearsInv: number, inv: number, yearsRet: number, retPct: number, divPct: number, divGrowth: number) {
+export default function calculate(yearsInv: number, yearsRet: number, inv: number, retPct: number, divPct: number, divGrowth: number) {
     interface year {
         year: number,
         investmentValue: number,
         dividends: number
     }
 
-    var yearlyTotals:number[] = []
+    interface yearValue {
+        year:  number,
+        value: number
+    }
+
+    var yearlyTotals:yearValue[] = []
     var yearlyInvestments:year[] = []
 
     /*
     set yearlyInvestments starting year, investment value, and dividends
     */
     for (var i = 1; i <= yearsInv; i++) {
-        yearlyInvestments.push({year: i, investmentValue: inv, dividends: inv * divPct});
+        yearlyInvestments.push({year: i, investmentValue: inv, dividends: inv * (divPct * .01)});
     }
 
     console.log(yearlyInvestments);
